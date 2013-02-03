5364c2ba8c2995dbdf056f7b3b496c47fe60fe77
Kevin Brown <bass5098@gmail.com>
2013-02-02 21:06:33 -0500

    Converted colors to a static class

--- BlockData/Colors.cs
+++ BlockData/Colors.cs
@@ -2,204 +2,204 @@
 
 namespace BlockData
 {
-    public enum Colors
+    public class Colors
     {
         // Basic Blocks
 
-        BlockBasicBlack = Color.FromArgb((int)1644825),
-        BlockBasicDarkBlue = Color.FromArgb((int)4281684648),
-        BlockBasicGreen = Color.FromArgb((int)4282558518),
-        BlockBasicGrey = Color.FromArgb((int)4285427310),
-        BlockBasicLightBlue = Color.FromArgb((int)4281704102),
-        BlockBasicPurple = Color.FromArgb((int)4288099751),
-        BlockBasicRed = Color.FromArgb((int)4289213780),
-        BlockBasicYellow = Color.FromArgb((int)4287866933),
+        public static Color BlockBasicBlack = Color.FromArgb((int)1644825);
+        public static Color BlockBasicDarkBlue = Color.FromArgb((int)4281684648);
+        public static Color BlockBasicGreen = Color.FromArgb((int)4282558518);
+        public static Color BlockBasicGrey = Color.FromArgb((int)4285427310);
+        public static Color BlockBasicLightBlue = Color.FromArgb((int)4281704102);
+        public static Color BlockBasicPurple = Color.FromArgb((int)4288099751);
+        public static Color BlockBasicRed = Color.FromArgb((int)4289213780);
+        public static Color BlockBasicYellow = Color.FromArgb((int)4287866933);
 
         // Beta Blocks
-
-        BlockBetaBlue = Color.FromArgb((int)4283270342),
-        BlockBetaGreen = Color.FromArgb((int)4283091074),
-        BlockBetaGrey = Color.FromArgb((int)4288321167),
-        BlockBetaPink = Color.FromArgb((int)4291715791),
-        BlockBetaRed = Color.FromArgb((int)4291782224),
-        BlockBetaYellow = Color.FromArgb((int)4291995973),
+        
+        public static Color BlockBetaBlue = Color.FromArgb((int)4283270342);
+        public static Color BlockBetaGreen = Color.FromArgb((int)4283091074);
+        public static Color BlockBetaGrey = Color.FromArgb((int)4288321167);
+        public static Color BlockBetaPink = Color.FromArgb((int)4291715791);
+        public static Color BlockBetaRed = Color.FromArgb((int)4291782224);
+        public static Color BlockBetaYellow = Color.FromArgb((int)4291995973);
 
         // Boost Blocks
 
-        BlockBoostDown = Color.FromArgb((int)16777216),
-        BlockBoostLeft = Color.FromArgb((int)16777216),
-        BlockBoostRight = Color.FromArgb((int)16777216),
-        BlockBoostUp = Color.FromArgb((int)16777216),
+        public static Color BlockBoostDown = Color.FromArgb((int)16777216);
+        public static Color BlockBoostLeft = Color.FromArgb((int)16777216);
+        public static Color BlockBoostRight = Color.FromArgb((int)16777216);
+        public static Color BlockBoostUp = Color.FromArgb((int)16777216);
 
         // Brick Blocks
 
-        BlockBrickDarkGreen = Color.FromArgb((int)4280577869),
-        BlockBrickLightGreen = Color.FromArgb((int)4282614544),
-        BlockBrickPaleBrown = Color.FromArgb((int)4285488420),
-        BlockBrickPurple = Color.FromArgb((int)4283311215),
-        BlockBrickRed = Color.FromArgb((int)4285473833),
-        BlockBrickSaturatedBrown = Color.FromArgb((int)4287315465),
+        public static Color BlockBrickDarkGreen = Color.FromArgb((int)4280577869);
+        public static Color BlockBrickLightGreen = Color.FromArgb((int)4282614544);
+        public static Color BlockBrickPaleBrown = Color.FromArgb((int)4285488420);
+        public static Color BlockBrickPurple = Color.FromArgb((int)4283311215);
+        public static Color BlockBrickRed = Color.FromArgb((int)4285473833);
+        public static Color BlockBrickSaturatedBrown = Color.FromArgb((int)4287315465);
 
         // Candy Blocks
 
-        BlockCandyOneWayBlue = Color.FromArgb((int)3232612),
-        BlockCandyOneWayGreen = Color.FromArgb((int)1263891),
-        BlockCandyOneWayPink = Color.FromArgb((int)7810156),
-        BlockCandyOneWayRed = Color.FromArgb((int)7411232),
-        BlockCandySolidChocolate = Color.FromArgb((int)9253137),
-        BlockCandySolidPink = Color.FromArgb((int)16487348),
-        BlockCandySolidStripeyRedWhite = Color.FromArgb((int)12743796),
-        BlockCandySolidStripeyYellowBlackPink = Color.FromArgb((int)10774865),
+        public static Color BlockCandyOneWayBlue = Color.FromArgb((int)3232612);
+        public static Color BlockCandyOneWayGreen = Color.FromArgb((int)1263891);
+        public static Color BlockCandyOneWayPink = Color.FromArgb((int)7810156);
+        public static Color BlockCandyOneWayRed = Color.FromArgb((int)7411232);
+        public static Color BlockCandySolidChocolate = Color.FromArgb((int)9253137);
+        public static Color BlockCandySolidPink = Color.FromArgb((int)16487348);
+        public static Color BlockCandySolidStripeyRedWhite = Color.FromArgb((int)12743796);
+        public static Color BlockCandySolidStripeyYellowBlackPink = Color.FromArgb((int)10774865);
 
         // Christmas 2011
 
-        BlockChristmas2011BlueBox = Color.FromArgb((int)5642622),
-        BlockChristmas2011GreenBox = Color.FromArgb((int)5670165),
-        BlockChristmas2011RedBox = Color.FromArgb((int)10044431),
-        BlockChristmas2011WhiteBox = Color.FromArgb((int)12814468),
-        BlockChristmas2011YellowBox = Color.FromArgb((int)8306196),
+        public static Color BlockChristmas2011BlueBox = Color.FromArgb((int)5642622);
+        public static Color BlockChristmas2011GreenBox = Color.FromArgb((int)5670165);
+        public static Color BlockChristmas2011RedBox = Color.FromArgb((int)10044431);
+        public static Color BlockChristmas2011WhiteBox = Color.FromArgb((int)12814468);
+        public static Color BlockChristmas2011YellowBox = Color.FromArgb((int)8306196);
 
         // Coins
 
-        BlockCoinBlue = Color.FromArgb((int)16777216),
-        BlockCoinGold = Color.FromArgb((int)16777216),
+        public static Color BlockCoinBlue = Color.FromArgb((int)16777216);
+        public static Color BlockCoinGold = Color.FromArgb((int)16777216);
 
         // Crown
 
-        BlockCrown = Color.FromArgb((int)4282595615),
+        public static Color BlockCrown = Color.FromArgb((int)4282595615);
 
         // Doors
 
-        BlockDoorBlue = Color.FromArgb((int)4281156764),
-        BlockDoorCoinDoor = Color.FromArgb((int)4290285077),
-        BlockDoorGreen = Color.FromArgb((int)4281834544),
-        BlockDoorPurpleSwitchable = Color.FromArgb((int)7031201),
-        BlockDoorRed = Color.FromArgb((int)4288425286),
-        BlockDoorTimedDoor = Color.FromArgb((int)4079166),
+        public static Color BlockDoorBlue = Color.FromArgb((int)4281156764);
+        public static Color BlockDoorCoinDoor = Color.FromArgb((int)4290285077);
+        public static Color BlockDoorGreen = Color.FromArgb((int)4281834544);
+        public static Color BlockDoorPurpleSwitchable = Color.FromArgb((int)7031201);
+        public static Color BlockDoorRed = Color.FromArgb((int)4288425286);
+        public static Color BlockDoorTimedDoor = Color.FromArgb((int)4079166);
 
         // Factory Blocks
 
-        BlockFactory1 = Color.FromArgb((int)4285686091),
-        BlockFactory2 = Color.FromArgb((int)4285426528),
-        BlockFactory3 = Color.FromArgb((int)4287525711),
-        BlockFactory4 = Color.FromArgb((int)4286533419),
-        BlockFactory5 = Color.FromArgb((int)4285887861),
+        public static Color BlockFactory1 = Color.FromArgb((int)4285686091);
+        public static Color BlockFactory2 = Color.FromArgb((int)4285426528);
+        public static Color BlockFactory3 = Color.FromArgb((int)4287525711);
+        public static Color BlockFactory4 = Color.FromArgb((int)4286533419);
+        public static Color BlockFactory5 = Color.FromArgb((int)4285887861);
 
         // Gates
 
-        BlockGateBlue = Color.FromArgb((int)4281156764),
-        BlockGateCoinGate = Color.FromArgb((int)12094997),
-        BlockGateGreen = Color.FromArgb((int)4281834544),
-        BlockGatePurpleSwitchable = Color.FromArgb((int)2104613),
-        BlockGateRed = Color.FromArgb((int)4288425286),
-        BlockGateTimedGate = Color.FromArgb((int)6184542),
+        public static Color BlockGateBlue = Color.FromArgb((int)4281156764);
+        public static Color BlockGateCoinGate = Color.FromArgb((int)12094997);
+        public static Color BlockGateGreen = Color.FromArgb((int)4281834544);
+        public static Color BlockGatePurpleSwitchable = Color.FromArgb((int)2104613);
+        public static Color BlockGateRed = Color.FromArgb((int)4288425286);
+        public static Color BlockGateTimedGate = Color.FromArgb((int)6184542);
 
         // Glass Blocks
 
-        BlockGlassDarkBlue = Color.FromArgb((int)4286487030),
-        BlockGlassGreen = Color.FromArgb((int)4287822762),
-        BlockGlassLightBlue = Color.FromArgb((int)4288011510),
-        BlockGlassOrange = Color.FromArgb((int)4294359700),
-        BlockGlassPink = Color.FromArgb((int)4293495798),
-        BlockGlassPurple = Color.FromArgb((int)4289169910),
-        BlockGlassRed = Color.FromArgb((int)4294480537),
-        BlockGlassYellow = Color.FromArgb((int)4294498956),
+        public static Color BlockGlassDarkBlue = Color.FromArgb((int)4286487030);
+        public static Color BlockGlassGreen = Color.FromArgb((int)4287822762);
+        public static Color BlockGlassLightBlue = Color.FromArgb((int)4288011510);
+        public static Color BlockGlassOrange = Color.FromArgb((int)4294359700);
+        public static Color BlockGlassPink = Color.FromArgb((int)4293495798);
+        public static Color BlockGlassPurple = Color.FromArgb((int)4289169910);
+        public static Color BlockGlassRed = Color.FromArgb((int)4294480537);
+        public static Color BlockGlassYellow = Color.FromArgb((int)4294498956);
 
         // Grass Blocks
 
-        BlockGrass1 = Color.FromArgb((int)4547347),
-        BlockGrass2 = Color.FromArgb((int)4547347),
-        BlockGrass3 = Color.FromArgb((int)4547347),
+        public static Color BlockGrass1 = Color.FromArgb((int)4547347);
+        public static Color BlockGrass2 = Color.FromArgb((int)4547347);
+        public static Color BlockGrass3 = Color.FromArgb((int)4547347);
 
         // Gravity Blocks
 
-        BlockGravityDot = Color.FromArgb((int)16777216),
-        BlockGravityLeft = Color.FromArgb((int)16777216),
-        BlockGravityNothing = Color.FromArgb((int)16777216),
-        BlockGravityRight = Color.FromArgb((int)16777216),
-        BlockGravityUp = Color.FromArgb((int)16777216),
+        public static Color BlockGravityDown = Color.FromArgb((int)16777216);
+        public static Color BlockGravityLeft = Color.FromArgb((int)16777216);
+        public static Color BlockGravityNothing = Color.FromArgb((int)16777216);
+        public static Color BlockGravityRight = Color.FromArgb((int)16777216);
+        public static Color BlockGravityUp = Color.FromArgb((int)16777216);
 
         // Halloween 2011
 
-        BlockHalloween20111 = Color.FromArgb((int)6837332),
-        BlockHalloween20112 = Color.FromArgb((int)6188660),
+        public static Color BlockHalloween20111 = Color.FromArgb((int)6837332);
+        public static Color BlockHalloween20112 = Color.FromArgb((int)6188660);
 
         // Key Blocks
 
-        BlockKeyBlue = Color.FromArgb((int)4279900716),
-        BlockKeyGreen = Color.FromArgb((int)4279905306),
-        BlockKeyRed = Color.FromArgb((int)4281080346),
+        public static Color BlockKeyBlue = Color.FromArgb((int)4279900716);
+        public static Color BlockKeyGreen = Color.FromArgb((int)4279905306);
+        public static Color BlockKeyRed = Color.FromArgb((int)4281080346);
 
         // Metal Blocks
 
-        BlockMetalRed = Color.FromArgb((int)4292835905),
-        BlockMetalWhite = Color.FromArgb((int)4288783269),
-        BlockMetalYellow = Color.FromArgb((int)4293962023),
+        public static Color BlockMetalRed = Color.FromArgb((int)4292835905);
+        public static Color BlockMetalWhite = Color.FromArgb((int)4288783269);
+        public static Color BlockMetalYellow = Color.FromArgb((int)4293962023);
 
         // Mineral Blocks
 
-        BlockMineralDarkBlue = Color.FromArgb((int)4278190318),
-        BlockMineralGreen = Color.FromArgb((int)4278251008),
-        BlockMineralLightBlue = Color.FromArgb((int)4278251246),
-        BlockMineralOrange = Color.FromArgb((int)4293818112),
-        BlockMineralPink = Color.FromArgb((int)4293787886),
-        BlockMineralRed = Color.FromArgb((int)4293787648),
-        BlockMineralYellow = Color.FromArgb((int)4293848576),
+        public static Color BlockMineralDarkBlue = Color.FromArgb((int)4278190318);
+        public static Color BlockMineralGreen = Color.FromArgb((int)4278251008);
+        public static Color BlockMineralLightBlue = Color.FromArgb((int)4278251246);
+        public static Color BlockMineralOrange = Color.FromArgb((int)4293818112);
+        public static Color BlockMineralPink = Color.FromArgb((int)4293787886);
+        public static Color BlockMineralRed = Color.FromArgb((int)4293787648);
+        public static Color BlockMineralYellow = Color.FromArgb((int)4293848576);
 
         // Music Blocks
 
-        BlockMusicDrum = Color.FromArgb((int)16777216),
-        BlockMusicPiano = Color.FromArgb((int)16777216),
+        public static Color BlockMusicDrum = Color.FromArgb((int)16777216);
+        public static Color BlockMusicPiano = Color.FromArgb((int)16777216);
 
         // Ninja Blocks
 
-        BlockNinjaGrey = Color.FromArgb((int)16777216),
-        BlockNinjaWhite = Color.FromArgb((int)16777216),
+        public static Color BlockNinjaGrey = Color.FromArgb((int)16777216);
+        public static Color BlockNinjaWhite = Color.FromArgb((int)16777216);
 
         // Pirate Blocks
 
-        BlockPirate1 = Color.FromArgb((int)11572068),
-        BlockPirateChest = Color.FromArgb((int)11048021),
+        public static Color BlockPirate1 = Color.FromArgb((int)11572068);
+        public static Color BlockPirateChest = Color.FromArgb((int)11048021);
 
         // Prison Block
 
-        BlockPrison = Color.FromArgb((int)8421504),
+        public static Color BlockPrison = Color.FromArgb((int)8421504);
 
         // Sci-Fi Blocks
 
-        BlockSciFiOneWayBlue = Color.FromArgb((int)8227772),
-        BlockSciFiOneWayGreen = Color.FromArgb((int)7911811),
-        BlockSciFiOneWayRed = Color.FromArgb((int)12352645),
-        BlockSciFiSolidBlue = Color.FromArgb((int)3895965),
-        BlockSciFiSolidBrown = Color.FromArgb((int)7098156),
-        BlockSciFiSolidGrey = Color.FromArgb((int)8816262),
-        BlockSciFiSolidRed = Color.FromArgb((int)10437440),
-        BlockSciFiSolidWhite = Color.FromArgb((int)16777215),
+        public static Color BlockSciFiOneWayBlue = Color.FromArgb((int)8227772);
+        public static Color BlockSciFiOneWayGreen = Color.FromArgb((int)7911811);
+        public static Color BlockSciFiOneWayRed = Color.FromArgb((int)12352645);
+        public static Color BlockSciFiSolidBlue = Color.FromArgb((int)3895965);
+        public static Color BlockSciFiSolidBrown = Color.FromArgb((int)7098156);
+        public static Color BlockSciFiSolidGrey = Color.FromArgb((int)8816262);
+        public static Color BlockSciFiSolidRed = Color.FromArgb((int)10437440);
+        public static Color BlockSciFiSolidWhite = Color.FromArgb((int)16777215);
 
         // Secret Blocks
 
-        BlockSecretPassable = Color.FromArgb((int)16777216),
-        BlockSecretUnpassable = Color.FromArgb((int)16777216),
+        public static Color BlockSecretPassable = Color.FromArgb((int)16777216);
+        public static Color BlockSecretUnpassable = Color.FromArgb((int)16777216);
 
         // Special Blocks
 
-        BlockSpecial1 = Color.FromArgb((int)4287191826),
-        BlockSpecial2 = Color.FromArgb((int)4291792930),
-        BlockSpecialNormalBlack = Color.FromArgb((int)16777216),
-        BlockSpecialFullyBlack = Color.FromArgb((int)16777216),
+        public static Color BlockSpecial1 = Color.FromArgb((int)4287191826);
+        public static Color BlockSpecial2 = Color.FromArgb((int)4291792930);
+        public static Color BlockSpecialNormalBlack = Color.FromArgb((int)16777216);
+        public static Color BlockSpecialFullyBlack = Color.FromArgb((int)16777216);
 
         // Summer 2011 Blocks
 
-        BlockSummer2011Sand = Color.FromArgb((int)14269318),
-        BlockSummer2011Sunshade = Color.FromArgb((int)16777216),
+        public static Color BlockSummer2011Sand = Color.FromArgb((int)14269318);
+        public static Color BlockSummer2011Sunshade = Color.FromArgb((int)16777216);
 
         // Switch Block
 
-        BlockSwitchPurple = Color.FromArgb((int)3749188),
+        public static Color BlockSwitchPurple = Color.FromArgb((int)3749188);
 
         // Viking Block
 
-        BlockViking = Color.FromArgb((int)6908007),
+        public static Color BlockViking = Color.FromArgb((int)6908007);
 
         // Name = Color.FromArgb((int)color_value),
     }
