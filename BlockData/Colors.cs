a3966997f5f7b07ea296aa99cd851873c6a752b1
Kevin Brown <bass5098@gmail.com>
2013-02-02 20:01:27 -0500

    Organized the colors and added some more

--- BlockData/Colors.cs
+++ BlockData/Colors.cs
@@ -4,27 +4,78 @@ namespace BlockData
 {
     public enum Colors
     {
-        BlockBasicGrey = Color.FromArgb((int)4285427310),
+        // Basic Blocks
+
+        BlockBasicBlack = Color.FromArgb((int)1644825),
         BlockBasicDarkBlue = Color.FromArgb((int)4281684648),
+        BlockBasicGreen = Color.FromArgb((int)4282558518),
+        BlockBasicGrey = Color.FromArgb((int)4285427310),
+        BlockBasicLightBlue = Color.FromArgb((int)4281704102),
         BlockBasicPurple = Color.FromArgb((int)4288099751),
         BlockBasicRed = Color.FromArgb((int)4289213780),
         BlockBasicYellow = Color.FromArgb((int)4287866933),
-        BlockBasicGreen = Color.FromArgb((int)4282558518),
-        BlockBasicLightBlue = Color.FromArgb((int)4281704102),
-        
-        BlockBrickSaturatedBrown = Color.FromArgb((int)4287315465),
-        BlockBrickDarkGreen = Color.FromArgb((int)4280577869),
-        BlockBrickPurple = Color.FromArgb((int)4283311215),
-        BlockBrickLightGreen = Color.FromArgb((int)4282614544),
-        BlockBrickRed = Color.FromArgb((int)4285473833),
-        BlockBrickPaleBrown = Color.FromArgb((int)4285488420),
 
-        BlockBetaPink = Color.FromArgb((int)4291715791),
-        BlockBetaGreen = Color.FromArgb((int)4283091074),
+        // Beta Blocks
+
         BlockBetaBlue = Color.FromArgb((int)4283270342),
+        BlockBetaGreen = Color.FromArgb((int)4283091074),
+        BlockBetaGrey = Color.FromArgb((int)4288321167),
+        BlockBetaPink = Color.FromArgb((int)4291715791),
         BlockBetaRed = Color.FromArgb((int)4291782224),
         BlockBetaYellow = Color.FromArgb((int)4291995973),
-        BlockBetaGrey = Color.FromArgb((int)4288321167),
+
+        // ?
+
+        BlockBrickDarkGreen = Color.FromArgb((int)4280577869),
+        BlockBrickLightGreen = Color.FromArgb((int)4282614544),
+        BlockBrickPaleBrown = Color.FromArgb((int)4285488420),
+        BlockBrickPurple = Color.FromArgb((int)4283311215),
+        BlockBrickRed = Color.FromArgb((int)4285473833),
+        BlockBrickSaturatedBrown = Color.FromArgb((int)4287315465),
+
+        // Doors
+
+        BlockDoorBlue = Color.FromArgb((int)4281156764),
+        BlockDoorCoinDoor = Color.FromArgb((int)4290285077),
+        BlockDoorGreen = Color.FromArgb((int)4281834544),
+        BlockDoorPurpleSwitchable = Color.FromArgb((int)7031201),
+        BlockDoorRed = Color.FromArgb((int)4288425286),
+        BlockDoorTimedDoor = Color.FromArgb((int)4079166),
+
+        // Gates
+
+        BlockGateBlue = Color.FromArgb((int)4281156764),
+        BlockGateCoinGate = Color.FromArgb((int)12094997),
+        BlockGateGreen = Color.FromArgb((int)4281834544),
+        BlockGateRed = Color.FromArgb((int)4288425286),
+        BlockGateTimedGate = Color.FromArgb((int)6184542),
+
+        // Grass Blocks
+
+        BlockGrass1 = Color.FromArgb((int)4547347),
+        BlockGrass2 = Color.FromArgb((int)4547347),
+        BlockGrass3 = Color.FromArgb((int)4547347),
+
+        // Gravity Blocks
+
+        BlockGravityNothing = Color.FromArgb((int)16777216),
+        BlockGravityLeft = Color.FromArgb((int)16777216),
+        BlockGravityUp = Color.FromArgb((int)16777216),
+        BlockGravityRight = Color.FromArgb((int)16777216),
+        BlockGravityDot = Color.FromArgb((int)16777216),
+
+        // Metal Blocks
+
+        BlockMetalRed = Color.FromArgb((int)4292835905),
+        BlockMetalWhite = Color.FromArgb((int)4288783269),
+        BlockMetalYellow = Color.FromArgb((int)4293962023),
+
+        // Special Blocks
+
+        BlockSpecial1 = Color.FromArgb((int)4287191826),
+        BlockSpecial2 = Color.FromArgb((int)4291792930),
+        BlockSpecialNormalBlack = Color.FromArgb((int)16777216),
+        BlockSpecialFullyBlack = Color.FromArgb((int)16777216),
 
         // Name = Color.FromArgb((int)color_value),
     }
