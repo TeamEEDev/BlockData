17ac6d2f9919219679ef8df42b196d1fc0bf32ae
Express50 <press.ex50@gmail.com>
2013-02-02 21:12:25 -0500

    Readonly

--- BlockData/Images.cs
+++ BlockData/Images.cs
@@ -5,130 +5,130 @@ namespace BlockData
     public class Images
     {
         //Gravity
-        public static Bitmap BlockGravityNothing = new Bitmap(BlockData.Resources.b0);
-        public static Bitmap BlockGravityLeft = new Bitmap(BlockData.Resources.b1);
-        public static Bitmap BlockGravityUp = new Bitmap(BlockData.Resources.b2);
-        public static Bitmap BlockGravityRight = new Bitmap(BlockData.Resources.b3);
-        public static Bitmap BlockGravityDot = new Bitmap(BlockData.Resources.b4);
+        public static readonly Bitmap BlockGravityNothing = new Bitmap(BlockData.Resources.b0);
+        public static readonly Bitmap BlockGravityLeft = new Bitmap(BlockData.Resources.b1);
+        public static readonly Bitmap BlockGravityUp = new Bitmap(BlockData.Resources.b2);
+        public static readonly Bitmap BlockGravityRight = new Bitmap(BlockData.Resources.b3);
+        public static readonly Bitmap BlockGravityDot = new Bitmap(BlockData.Resources.b4);
 
         //Crown
-        public static Bitmap BlockCrown = new Bitmap(BlockData.Resources.b5);
+        public static readonly Bitmap BlockCrown = new Bitmap(BlockData.Resources.b5);
 
         //Keys
-        public static Bitmap BlockKeyRed = new Bitmap(BlockData.Resources.b6);
-        public static Bitmap BlockKeyGreen = new Bitmap(BlockData.Resources.b7);
-        public static Bitmap BlockKeyBlue = new Bitmap(BlockData.Resources.b8);
+        public static readonly Bitmap BlockKeyRed = new Bitmap(BlockData.Resources.b6);
+        public static readonly Bitmap BlockKeyGreen = new Bitmap(BlockData.Resources.b7);
+        public static readonly Bitmap BlockKeyBlue = new Bitmap(BlockData.Resources.b8);
 
         //Basic Block Pack
-        public static Bitmap BlockBasicGrey = new Bitmap(BlockData.Resources.b9);
-        public static Bitmap BlockBasicBlue = new Bitmap(BlockData.Resources.b10);
-        public static Bitmap BlockBasicPurple = new Bitmap(BlockData.Resources.b11);
-        public static Bitmap BlockBasicRed = new Bitmap(BlockData.Resources.b12);
-        public static Bitmap BlockBasicYellow = new Bitmap(BlockData.Resources.b13);
-        public static Bitmap BlockBasicGreen = new Bitmap(BlockData.Resources.b14);
-        public static Bitmap BlockBasicLightBlue = new Bitmap(BlockData.Resources.b15);
-        public static Bitmap BlockBasicBlack = new Bitmap(BlockData.Resources.b182);
+        public static readonly Bitmap BlockBasicGrey = new Bitmap(BlockData.Resources.b9);
+        public static readonly Bitmap BlockBasicBlue = new Bitmap(BlockData.Resources.b10);
+        public static readonly Bitmap BlockBasicPurple = new Bitmap(BlockData.Resources.b11);
+        public static readonly Bitmap BlockBasicRed = new Bitmap(BlockData.Resources.b12);
+        public static readonly Bitmap BlockBasicYellow = new Bitmap(BlockData.Resources.b13);
+        public static readonly Bitmap BlockBasicGreen = new Bitmap(BlockData.Resources.b14);
+        public static readonly Bitmap BlockBasicLightBlue = new Bitmap(BlockData.Resources.b15);
+        public static readonly Bitmap BlockBasicBlack = new Bitmap(BlockData.Resources.b182);
 
         //Brick Pack
-        public static Bitmap BlockBrickSaturatedBrown = new Bitmap(BlockData.Resources.b16);
-        public static Bitmap BlockBrickDarkGreen = new Bitmap(BlockData.Resources.b17);
-        public static Bitmap BlockBrickPurple = new Bitmap(BlockData.Resources.b18);
-        public static Bitmap BlockBrickLightGreen = new Bitmap(BlockData.Resources.b19);
-        public static Bitmap BlockBrickRed = new Bitmap(BlockData.Resources.b20);
-        public static Bitmap BlockBrickPaleBrown = new Bitmap(BlockData.Resources.b21);
+        public static readonly Bitmap BlockBrickSaturatedBrown = new Bitmap(BlockData.Resources.b16);
+        public static readonly Bitmap BlockBrickDarkGreen = new Bitmap(BlockData.Resources.b17);
+        public static readonly Bitmap BlockBrickPurple = new Bitmap(BlockData.Resources.b18);
+        public static readonly Bitmap BlockBrickLightGreen = new Bitmap(BlockData.Resources.b19);
+        public static readonly Bitmap BlockBrickRed = new Bitmap(BlockData.Resources.b20);
+        public static readonly Bitmap BlockBrickPaleBrown = new Bitmap(BlockData.Resources.b21);
 
         //Special Blocks Pack
-        public static Bitmap BlockSpecial1 = new Bitmap(BlockData.Resources.b22);
-        public static Bitmap BlockSpecial2 = new Bitmap(BlockData.Resources.b32);
-        public static Bitmap BlockSpecialNormalBlack = new Bitmap(BlockData.Resources.b33);
-        public static Bitmap BlockSpecialFullyBlack = new Bitmap(BlockData.Resources.b44);
+        public static readonly Bitmap BlockSpecial1 = new Bitmap(BlockData.Resources.b22);
+        public static readonly Bitmap BlockSpecial2 = new Bitmap(BlockData.Resources.b32);
+        public static readonly Bitmap BlockSpecialNormalBlack = new Bitmap(BlockData.Resources.b33);
+        public static readonly Bitmap BlockSpecialFullyBlack = new Bitmap(BlockData.Resources.b44);
 
         //Doors Pack
-        public static Bitmap BlockDoorRed = new Bitmap(BlockData.Resources.b23);
-        public static Bitmap BlockDoorGreen = new Bitmap(BlockData.Resources.b24);
-        public static Bitmap BlockDoorBlue = new Bitmap(BlockData.Resources.b25);
-        public static Bitmap BlockDoorCoinDoor = new Bitmap(BlockData.Resources.b43);
-        public static Bitmap BlockDoorTimedDoor = new Bitmap(BlockData.Resources.b156);
-        public static Bitmap BlockDoorPurpleSwitchable = new Bitmap(BlockData.Resources.b184);
+        public static readonly Bitmap BlockDoorRed = new Bitmap(BlockData.Resources.b23);
+        public static readonly Bitmap BlockDoorGreen = new Bitmap(BlockData.Resources.b24);
+        public static readonly Bitmap BlockDoorBlue = new Bitmap(BlockData.Resources.b25);
+        public static readonly Bitmap BlockDoorCoinDoor = new Bitmap(BlockData.Resources.b43);
+        public static readonly Bitmap BlockDoorTimedDoor = new Bitmap(BlockData.Resources.b156);
+        public static readonly Bitmap BlockDoorPurpleSwitchable = new Bitmap(BlockData.Resources.b184);
 
         //Gates Pack
-        public static Bitmap BlockGateRed = new Bitmap(BlockData.Resources.b26);
-        public static Bitmap BlockGateGreen = new Bitmap(BlockData.Resources.b27);
-        public static Bitmap BlockGateBlue = new Bitmap(BlockData.Resources.b28);
-        public static Bitmap BlockGateTimedGate = new Bitmap(BlockData.Resources.b157);
-        public static Bitmap BlockGateCoinGate = new Bitmap(BlockData.Resources.b165);
+        public static readonly Bitmap BlockGateRed = new Bitmap(BlockData.Resources.b26);
+        public static readonly Bitmap BlockGateGreen = new Bitmap(BlockData.Resources.b27);
+        public static readonly Bitmap BlockGateBlue = new Bitmap(BlockData.Resources.b28);
+        public static readonly Bitmap BlockGateTimedGate = new Bitmap(BlockData.Resources.b157);
+        public static readonly Bitmap BlockGateCoinGate = new Bitmap(BlockData.Resources.b165);
         //BlockGatePurpleSwitchable id=185
 
         //Metal Pack
-        public static Bitmap BlockMetalWhite = new Bitmap(BlockData.Resources.b29);
-        public static Bitmap BlockMetalRed = new Bitmap(BlockData.Resources.b30);
-        public static Bitmap BlockMetalYellow = new Bitmap(BlockData.Resources.b31);
+        public static readonly Bitmap BlockMetalWhite = new Bitmap(BlockData.Resources.b29);
+        public static readonly Bitmap BlockMetalRed = new Bitmap(BlockData.Resources.b30);
+        public static readonly Bitmap BlockMetalYellow = new Bitmap(BlockData.Resources.b31);
 
         //Grass Pack
-        public static Bitmap BlockGrass1 = new Bitmap(BlockData.Resources.b34);
-        public static Bitmap BlockGrass2 = new Bitmap(BlockData.Resources.b35);
-        public static Bitmap BlockGrass3 = new Bitmap(BlockData.Resources.b36);
+        public static readonly Bitmap BlockGrass1 = new Bitmap(BlockData.Resources.b34);
+        public static readonly Bitmap BlockGrass2 = new Bitmap(BlockData.Resources.b35);
+        public static readonly Bitmap BlockGrass3 = new Bitmap(BlockData.Resources.b36);
 
         //Beta Pack
-        public static Bitmap BlockBetaPink = new Bitmap(BlockData.Resources.b37);
-        public static Bitmap BlockBetaGreen = new Bitmap(BlockData.Resources.b38);
-        public static Bitmap BlockBetaBlue = new Bitmap(BlockData.Resources.b39);
-        public static Bitmap BlockBetaRed = new Bitmap(BlockData.Resources.b40);
-        public static Bitmap BlockBetaYellow = new Bitmap(BlockData.Resources.b41);
-        public static Bitmap BlockBetaGrey = new Bitmap(BlockData.Resources.b42);
+        public static readonly Bitmap BlockBetaPink = new Bitmap(BlockData.Resources.b37);
+        public static readonly Bitmap BlockBetaGreen = new Bitmap(BlockData.Resources.b38);
+        public static readonly Bitmap BlockBetaBlue = new Bitmap(BlockData.Resources.b39);
+        public static readonly Bitmap BlockBetaRed = new Bitmap(BlockData.Resources.b40);
+        public static readonly Bitmap BlockBetaYellow = new Bitmap(BlockData.Resources.b41);
+        public static readonly Bitmap BlockBetaGrey = new Bitmap(BlockData.Resources.b42);
 
         //Factory Pack
-        public static Bitmap BlockFactory1 = new Bitmap(BlockData.Resources.b45);
-        public static Bitmap BlockFactory2 = new Bitmap(BlockData.Resources.b46);
-        public static Bitmap BlockFactory3 = new Bitmap(BlockData.Resources.b47);
-        public static Bitmap BlockFactory4 = new Bitmap(BlockData.Resources.b48);
-        public static Bitmap BlockFactory5 = new Bitmap(BlockData.Resources.b49);
+        public static readonly Bitmap BlockFactory1 = new Bitmap(BlockData.Resources.b45);
+        public static readonly Bitmap BlockFactory2 = new Bitmap(BlockData.Resources.b46);
+        public static readonly Bitmap BlockFactory3 = new Bitmap(BlockData.Resources.b47);
+        public static readonly Bitmap BlockFactory4 = new Bitmap(BlockData.Resources.b48);
+        public static readonly Bitmap BlockFactory5 = new Bitmap(BlockData.Resources.b49);
 
         //Secrets
-        public static Bitmap BlockSecretUnpassable = new Bitmap(BlockData.Resources.b50);
-        public static Bitmap BlockSecretPassable = new Bitmap(BlockData.Resources.b243);
+        public static readonly Bitmap BlockSecretUnpassable = new Bitmap(BlockData.Resources.b50);
+        public static readonly Bitmap BlockSecretPassable = new Bitmap(BlockData.Resources.b243);
 
         //Glass Pack
-        public static Bitmap BlockGlassRed = new Bitmap(BlockData.Resources.b51);
-        public static Bitmap BlockGlassPink = new Bitmap(BlockData.Resources.b52);
-        public static Bitmap BlockGlassPurple = new Bitmap(BlockData.Resources.b53);
-        public static Bitmap BlockGlassDarkBlue = new Bitmap(BlockData.Resources.b54);
-        public static Bitmap BlockGlassLightBlue = new Bitmap(BlockData.Resources.b55);
-        public static Bitmap BlockGlassGreen = new Bitmap(BlockData.Resources.b56);
-        public static Bitmap BlockGlassYellow = new Bitmap(BlockData.Resources.b57);
-        public static Bitmap BlockGlassOrange = new Bitmap(BlockData.Resources.b58);
+        public static readonly Bitmap BlockGlassRed = new Bitmap(BlockData.Resources.b51);
+        public static readonly Bitmap BlockGlassPink = new Bitmap(BlockData.Resources.b52);
+        public static readonly Bitmap BlockGlassPurple = new Bitmap(BlockData.Resources.b53);
+        public static readonly Bitmap BlockGlassDarkBlue = new Bitmap(BlockData.Resources.b54);
+        public static readonly Bitmap BlockGlassLightBlue = new Bitmap(BlockData.Resources.b55);
+        public static readonly Bitmap BlockGlassGreen = new Bitmap(BlockData.Resources.b56);
+        public static readonly Bitmap BlockGlassYellow = new Bitmap(BlockData.Resources.b57);
+        public static readonly Bitmap BlockGlassOrange = new Bitmap(BlockData.Resources.b58);
         
         //Summer 2011 Pack
-        public static Bitmap BlockSummer2011Sand = new Bitmap(BlockData.Resources.b59);
+        public static readonly Bitmap BlockSummer2011Sand = new Bitmap(BlockData.Resources.b59);
 
         //Candy Pack
-        public static Bitmap BlockCandySolidPink = new Bitmap(BlockData.Resources.b60);
-        public static Bitmap BlockCandyOneWayPink = new Bitmap(BlockData.Resources.b61);
-        public static Bitmap BlockCandyOneWayRed = new Bitmap(BlockData.Resources.b62);
-        public static Bitmap BlockCandyOneWayBlue = new Bitmap(BlockData.Resources.b63);
-        public static Bitmap BlockCandyOneWayGreen = new Bitmap(BlockData.Resources.b64);
-        public static Bitmap BlockCandySolidStripeyRedWhite = new Bitmap(BlockData.Resources.b65);
-        public static Bitmap BlockCandySolidStripeyYellowBlackPink = new Bitmap(BlockData.Resources.b66);
-        public static Bitmap BlockCandySolidChocolate = new Bitmap(BlockData.Resources.b67);
+        public static readonly Bitmap BlockCandySolidPink = new Bitmap(BlockData.Resources.b60);
+        public static readonly Bitmap BlockCandyOneWayPink = new Bitmap(BlockData.Resources.b61);
+        public static readonly Bitmap BlockCandyOneWayRed = new Bitmap(BlockData.Resources.b62);
+        public static readonly Bitmap BlockCandyOneWayBlue = new Bitmap(BlockData.Resources.b63);
+        public static readonly Bitmap BlockCandyOneWayGreen = new Bitmap(BlockData.Resources.b64);
+        public static readonly Bitmap BlockCandySolidStripeyRedWhite = new Bitmap(BlockData.Resources.b65);
+        public static readonly Bitmap BlockCandySolidStripeyYellowBlackPink = new Bitmap(BlockData.Resources.b66);
+        public static readonly Bitmap BlockCandySolidChocolate = new Bitmap(BlockData.Resources.b67);
 
         //Halloween 2011 Pack
-        public static Bitmap BlockHalloween20111 = new Bitmap(BlockData.Resources.b68);
-        public static Bitmap BlockHalloween20112 = new Bitmap(BlockData.Resources.b69);
+        public static readonly Bitmap BlockHalloween20111 = new Bitmap(BlockData.Resources.b68);
+        public static readonly Bitmap BlockHalloween20112 = new Bitmap(BlockData.Resources.b69);
 
         //Mineral Pack
-        public static Bitmap BlockMineralRed = new Bitmap(BlockData.Resources.b70);
-        public static Bitmap BlockMineralPink = new Bitmap(BlockData.Resources.b71);
-        public static Bitmap BlockMineralDarkBlue = new Bitmap(BlockData.Resources.b72);
-        public static Bitmap BlockMineralLightBlue = new Bitmap(BlockData.Resources.b73);
-        public static Bitmap BlockMineralGreen = new Bitmap(BlockData.Resources.b74);
-        public static Bitmap BlockMineralYellow = new Bitmap(BlockData.Resources.b75);
-        public static Bitmap BlockMineralOrange = new Bitmap(BlockData.Resources.b76);
+        public static readonly Bitmap BlockMineralRed = new Bitmap(BlockData.Resources.b70);
+        public static readonly Bitmap BlockMineralPink = new Bitmap(BlockData.Resources.b71);
+        public static readonly Bitmap BlockMineralDarkBlue = new Bitmap(BlockData.Resources.b72);
+        public static readonly Bitmap BlockMineralLightBlue = new Bitmap(BlockData.Resources.b73);
+        public static readonly Bitmap BlockMineralGreen = new Bitmap(BlockData.Resources.b74);
+        public static readonly Bitmap BlockMineralYellow = new Bitmap(BlockData.Resources.b75);
+        public static readonly Bitmap BlockMineralOrange = new Bitmap(BlockData.Resources.b76);
 
         //Music Pack
-        public static Bitmap BlockMusicPiano = new Bitmap(BlockData.Resources.b77);
-        public static Bitmap BlockMusicDrum = new Bitmap(BlockData.Resources.b78);
+        public static readonly Bitmap BlockMusicPiano = new Bitmap(BlockData.Resources.b77);
+        public static readonly Bitmap BlockMusicDrum = new Bitmap(BlockData.Resources.b78);
 
 
-        //public static Bitmap Name = new Bitmap(BlockData.Resources.b[block-id]);
+        //public static readonly Bitmap Name = new Bitmap(BlockData.Resources.b[block-id]);
     }
 }
