b00f11007150ea286b27c595305e06b8147ebfed
Kevin Brown <bass5098@gmail.com>
2013-02-02 19:35:43 -0500

    Started adding colors

--- BlockData/Colors.cs
+++ BlockData/Colors.cs
@@ -5,5 +5,27 @@ namespace BlockData
     public enum Colors
     {
         BlockBasicGrey = Color.FromArgb((int)4285427310),
+        BlockBasicDarkBlue = Color.FromArgb((int)4281684648),
+        BlockBasicPurple = Color.FromArgb((int)4288099751),
+        BlockBasicRed = Color.FromArgb((int)4289213780),
+        BlockBasicYellow = Color.FromArgb((int)4287866933),
+        BlockBasicGreen = Color.FromArgb((int)4282558518),
+        BlockBasicLightBlue = Color.FromArgb((int)4281704102),
+        
+        BlockBrickSaturatedBrown = Color.FromArgb((int)4287315465),
+        BlockBrickDarkGreen = Color.FromArgb((int)4280577869),
+        BlockBrickPurple = Color.FromArgb((int)4283311215),
+        BlockBrickLightGreen = Color.FromArgb((int)4282614544),
+        BlockBrickRed = Color.FromArgb((int)4285473833),
+        BlockBrickPaleBrown = Color.FromArgb((int)4285488420),
+
+        BlockBetaPink = Color.FromArgb((int)4291715791),
+        BlockBetaGreen = Color.FromArgb((int)4283091074),
+        BlockBetaBlue = Color.FromArgb((int)4283270342),
+        BlockBetaRed = Color.FromArgb((int)4291782224),
+        BlockBetaYellow = Color.FromArgb((int)4291995973),
+        BlockBetaGrey = Color.FromArgb((int)4288321167),
+
+        // Name = Color.FromArgb((int)color_value),
     }
 }
